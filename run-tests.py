e740dba14db6c7e89c5ea2341f7b4ba50d8ad8c9
Graham Markall <graham.markall@embecosm.com>
2016-05-23 07:28:47 +0100

    Run all relevant tests from gcc and g++ suites

--- run-tests.py
+++ run-tests.py
@@ -23,6 +23,7 @@
 # completed successfully, and that gcc is cloned next to the other repositories
 # with the test-override branch checked out.
 
+from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from glob import glob
 from shutil import rmtree
@@ -42,8 +43,67 @@ OUTPUT_DIR = os.path.join(TOOLCHAIN, 'test-output')
 TEST_BOARD = 'aap-run'
 RUNTEST_FLAGS = []
 
-TEST_DIR = 'gcc.c-torture'
-TEST_SET = 'execute'
+TestSet = namedtuple('TestSet', ['tool', 'set_dir', 'sub_dir', 'expect_file'])
+
+TORTURE_TESTS = [
+    TestSet('gcc', 'gcc.c-torture', 'compile', 'compile.exp'),
+    TestSet('gcc', 'gcc.c-torture', 'execute', 'execute.exp'),
+    TestSet('gcc', 'gcc.c-torture', 'unsorted', 'unsorted.exp'),
+]
+
+# Tests from gcc.dg that we're not including:
+#
+# plugin:      GCC's plugin architecture
+# tm:          Transactional memory
+# tsan:        Thread Sanitizer
+# cilk-plus:   Cilk Plus
+# autopar:     Automatic parallelisation
+# fixed-point: Fixed-point arithmetic
+# vmx:         VMX instructions (for PowerPC)
+# pch:         Precompiled headers
+# dfp:         Decimal floating point
+# gomp:        GNU OpenMP
+# vxworks:     VxWorks targets
+# graphite:    GRAPHITE polyhedral optimisation
+# ipa:         Interprocedural analysis dumps
+# goacc:       GNU OpenACC support
+# goacc-gomp:  GNU OpenACC support
+# cpp/trad:    Traditional preprocessor mode
+# vect:        Vector instructions
+# torture:     <<< Need to look at this again >>>
+# charset:     IBM 1047 charset
+# compat:      Test compatibility of code from two compilers linked together
+# guality:     Test the quality of debug information
+# special:     ???
+# tree-ssa:    Test tree-ssa dumps
+# debug:       debug info
+# ubsan:       Undefined behaviour sanitizer
+# tree-prof:   Profile-directed block ordering
+# lto:         Link-time optimisation
+# asan:        Address sanitizer
+# wformat:     Warnings about format features
+# atomic:      C11 atomics
+
+DG_TESTS = [
+    # Tests in root dir of dg suite
+    TestSet('gcc', 'gcc.dg', '', 'dg.exp'),
+    # C Preprocessor
+    TestSet('gcc', 'gcc.dg', 'cpp', 'cpp.exp'),
+    # Tests atomicity/for race conditions
+    TestSet('gcc', 'gcc.dg', 'simulate-thread', 'simulate-thread.exp'),
+    # Things that should not compile
+    TestSet('gcc', 'gcc.dg', 'noncompile', 'noncompile.exp'),
+    # Weak symbols
+    TestSet('gcc', 'gcc.dg', 'weak', 'weak.exp'),
+    # Thread-local storage
+    TestSet('gcc', 'gcc.dg', 'tls', 'tls.exp'),
+]
+
+CXX_TESTS = [
+    TestSet('g++', 'g++.dg', '', 'dg.exp')
+]
+
+TEST_SETS = CXX_TESTS + TORTURE_TESTS + DG_TESTS
 
 # The number of tests to pass to a single Dejagnu invocation at once
 DG_INSTANCE_NTESTS = 20
@@ -54,21 +114,24 @@ WORKERS = os.cpu_count()
 
 class TestManager(object):
     '''
-    The TestManager maintains the list of the tests that need to be run.
+    The TestManager maintains the list of the tests that need to be run for a
+    particular test set.
     '''
-    def __init__(self):
+    def __init__(self, test_set):
         self._tests_lock = Lock()
         self._tests = None
         self._current_run = 0
+        self._test_set = test_set
 
-    def find_tests(self, root):
+    def find_tests(self):
         '''
-        Find all .c files under the path root, relative to root.
+        Find all .c files in the test set.
         '''
+        root = os.path.join(TEST_SUITE, self._test_set.set_dir, self._test_set.sub_dir)
         found = []
         for path, _, files in os.walk(root):
             found += [ os.path.relpath(os.path.join(path, name), start=root)
-                           for name in files if name[-2:] == '.c' ]
+                           for name in files if name[-2:] in ('.c', '.C') ]
         print("Discovered %s test files" % len(found))
         self._tests = found
 
@@ -95,34 +158,47 @@ class TestManager(object):
         self._tests_lock.release()
         return i, t
 
+    @property
+    def test_set(self):
+        return self._test_set
+
 
 def runtests_env():
     env = os.environ.copy()
     env['DEJAGNU'] = os.path.join(TOPDIR, "toolchain", "site.exp")
+    bindir = os.path.join(os.path.abspath('..'), 'install-master', 'bin')
+    path = '%s:%s' % (bindir, env['PATH'])
+    env['PATH'] = path
     return env
 
-def runtests(i, tests):
+def runtests(i, test_set, tests):
     '''
-    Launch instance i of DejaGnu, running the given list of tests
+    Launch instance i of DejaGnu, running the given list of tests in the
+    test_set.
     '''
+    set_dir = test_set.set_dir
+    sub_dir = test_set.sub_dir
+
     # Prepare output directory
-    dg_output_dir = os.path.join(OUTPUT_DIR, 'dgrun_%s' % i)
+    unique_name = '%s_%s_%s' % (set_dir, sub_dir, i)
+    dg_output_dir = os.path.join(OUTPUT_DIR, 'dgrun_%s' % unique_name)
     os.mkdir(dg_output_dir)
 
-    test_list = " ".join([ os.path.join(TEST_SET, test) for test in tests ])
+    test_list = " ".join([ os.path.join(test_set.sub_dir, test)
+                           for test in tests ])
     args = [
         'runtest',
-        '--tool=gcc',
-        '--directory=%s' % os.path.join(TEST_SUITE, TEST_DIR),
+        '--tool=%s' % test_set.tool,
+        '--directory=%s' % os.path.join(TEST_SUITE, set_dir),
         '--srcdir=%s' % TEST_SUITE,
         '--override_manifest=%s' % OVERRIDE_MANIFEST,
-        '%s.exp=%s' % (TEST_SET, test_list) ]
+        '%s=%s' % (test_set.expect_file, test_list) ]
     args += RUNTEST_FLAGS
 
     proc = Popen(args, env=runtests_env(), cwd=dg_output_dir)
     return proc.wait()
 
-def test_loop(tm=None):
+def test_loop(*, tm=None):
     '''
     Repeatedly invoke DejaGnu with a list of tests obtained from the TestManager.
 
@@ -133,7 +209,7 @@ def test_loop(tm=None):
 
     i, next_tests = tm.pop_tests(DG_INSTANCE_NTESTS)
     while next_tests:
-        runtests(i, next_tests)
+        runtests(i, tm.test_set, next_tests)
         i, next_tests = tm.pop_tests(DG_INSTANCE_NTESTS)
 
 
@@ -153,11 +229,19 @@ def combine_results(dg_outfile, *, extra_args=None):
         if result.wait():
             print("Warning: error combining %s" % dg_outfile, file=sys.stderr)
 
+def all_tools():
+    tools = set()
+    for ts in TEST_SETS:
+        tools.add(ts.tool)
+    return tools
+
 def combine_sums():
-    combine_results('gcc.sum')
+    for tool in all_tools():
+        combine_results('%s.sum' % tool)
 
 def combine_logs():
-    combine_results('gcc.log', extra_args=['-L'])
+    for tool in all_tools():
+        combine_results('%s.log' % tool, extra_args=['-L'])
 
 def print_summary():
     '''
@@ -175,14 +259,15 @@ def print_summary():
         '# of unresolved testcases',
         '# of unsupported tests' ]
 
-    with open(os.path.join(OUTPUT_DIR, 'gcc.sum')) as f:
-        summary = [
-            line for line in f
-            for phrase in interesting
-            if phrase in line ]
+    for tool in all_tools():
+        with open(os.path.join(OUTPUT_DIR, '%s.sum' % tool)) as f:
+            summary = [
+                line for line in f
+                for phrase in interesting
+                if phrase in line ]
 
-    print("\nTest summary:\n")
-    print("".join(summary))
+        print("\n%s summary:\n" % tool)
+        print("".join(summary))
 
 
 def main(args):
@@ -191,18 +276,30 @@ def main(args):
         rmtree(OUTPUT_DIR)
     os.mkdir(OUTPUT_DIR)
 
-    # Set up the test manager
-    tm = TestManager()
-    tm.find_tests(os.path.join(TEST_SUITE, TEST_DIR, TEST_SET))
+    for test_set in TEST_SETS:
+        # Set up the test manager
+        tm = TestManager(test_set)
+        tm.find_tests()
 
-    # Use a set of workers to execute tests in parallel
-    executor = ThreadPoolExecutor(max_workers=WORKERS)
-    jobs = {executor.submit(test_loop, **{'tm': tm}): i for i in range(WORKERS)}
-    for worker in as_completed(jobs):
-        try:
-            worker.result()
-        except Exception as exc:
-            print("Job %s generated an exception: %s" % (worker, exc))
+        # Use a set of workers to execute tests in parallel
+        executor = ThreadPoolExecutor(max_workers=WORKERS)
+        test_loop_args = { 'tm': tm }
+        jobs = { executor.submit(test_loop, **test_loop_args): i
+                 for i in range(WORKERS) }
+
+        # Wait on workers - halt if any errors from any of them
+        halt = False
+        for worker in as_completed(jobs):
+            try:
+                worker.result()
+            except Exception as exc:
+                import traceback
+                print("Job %s generated an exception: %s" % (worker, exc))
+                traceback.print_exc()
+                halt = True
+
+        if halt:
+            break
 
     # Combine the output from all instances
     combine_sums()
